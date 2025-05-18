0a13930ab3afb6551f95c6c0ed59b0a08e822dd9
VLADISLAW9 <duplinskij_v_d@mail.ru>
2025-05-18 15:44:17 +0700

    add eslint and prettier

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.css';
+	import { ClipboardCopy } from 'lucide-svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="h-screen flex justify-between flex-col">
+	<header class="flex justify-between px-30 py-5">
+		<div class="flex gap-2">
+			<ClipboardCopy />
+			<h1>Planner</h1>
+		</div>
+	</header>
+	<div class="px-30 flex-1">
+		{@render children()}
+	</div>
+	<footer class="flex justify-between px-30 py-5">footer</footer>
+</div>
